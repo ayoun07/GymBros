c7237c9172270793e8555eab1a9c747f01a00ff2
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-17 18:04:34 +0100

    first commit

---
+++ frontend/src/pages/home/Home.tsx
@@ -0,0 +1,188 @@
+import { NavLink } from "react-router-dom";
+
+function Home() {
+  return (
+    <div className="flex flex-col">
+      <section className="flex max-sm:flex-col justify-between items-center mb-16">
+        <p className="felx">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+          perspiciatis, similique voluptatum incidunt in soluta ea
+          necessitatibus voluptates illum libero modi, eveniet maiores natus,
+          eius itaque magnam voluptate neque cum!
+        </p>
+        <div className="w-fit shadow-2xl">
+          <img
+            className="rounded-2xl w-fit h-full bg-blue-200"
+            src="https://img.freepik.com/psd-premium/maquette-halteres-enduite-vinyle-vue-droite-gros-plan_1332-982.jpg?w=1480"
+            alt=""
+          />
+        </div>
+      </section>
+      <section className="gap-4 flex flex-col mb-32">
+        <h2 className="text-3xl font-medium">Nutrition </h2>
+        <div className="rounded-2xl p-4 flex flex-col gap-16 h-fit shadow-xl bg-[#f8f7f4]">
+          <div className="rounded-2xl p-2 h-fit shadow-xl bg-[#F8F7F4]">
+            <div className="text-center flex flex-col gap-4">
+              <img
+                className="shadow-2xl rounded-2xl w-full h-74 bg-fit object-cover"
+                src="https://img.freepik.com/psd-premium/boules-poids-pour-maquette-entrainement_439185-407.jpg?w=1480"
+                alt=""
+              />
+              <p className="text-2xl uppercase ">
+                Proteines maisons giehguiehg
+              </p>
+              <p className="text-[20px]">19.99€</p>
+              <NavLink
+                className="bg-[#5390b3] rounded-2xl text-white w-full px-2 py-4 uppercase font-medium"
+                to={"/"}
+              >
+                Voir les détails
+              </NavLink>
+            </div>
+          </div>
+          <div className="rounded-2xl p-2 h-fit shadow-xl bg-[#F8F7F4]">
+            <div className="text-center flex flex-col gap-4">
+              <img
+                className="shadow-2xl rounded-2xl w-full h-74 bg-fit object-cover"
+                src="https://img.freepik.com/psd-premium/boules-poids-pour-maquette-entrainement_439185-407.jpg?w=1480"
+                alt=""
+              />
+              <p className="text-2xl uppercase ">
+                Proteines maisons giehguiehg
+              </p>
+              <p className="text-[20px]">19.99€</p>
+              <NavLink
+                className="bg-[#5390b3] rounded-2xl text-white w-full px-2 py-4 uppercase font-medium"
+                to={"/"}
+              >
+                Voir les détails
+              </NavLink>
+            </div>
+          </div>
+          <div className="rounded-2xl p-2 h-fit shadow-xl bg-[#F8F7F4]">
+            <div className="text-center flex flex-col gap-4">
+              <img
+                className="shadow-2xl rounded-2xl w-full h-74 bg-fit object-cover"
+                src="https://img.freepik.com/psd-premium/boules-poids-pour-maquette-entrainement_439185-407.jpg?w=1480"
+                alt=""
+              />
+              <p className="text-2xl uppercase ">
+                Proteines maisons giehguiehg
+              </p>
+              <p className="text-[20px]">19.99€</p>
+              <NavLink
+                className="bg-[#5390b3] rounded-2xl text-white w-full px-2 py-4 uppercase font-medium"
+                to={"/"}
+              >
+                Voir les détails
+              </NavLink>
+            </div>
+          </div>
+          <div className="rounded-2xl p-2 h-fit shadow-xl bg-[#F8F7F4]">
+            <div className="text-center flex flex-col gap-4">
+              <img
+                className="shadow-2xl rounded-2xl w-full h-74 bg-fit object-cover"
+                src="https://img.freepik.com/psd-premium/boules-poids-pour-maquette-entrainement_439185-407.jpg?w=1480"
+                alt=""
+              />
+              <p className="text-2xl uppercase ">
+                Proteines maisons giehguiehg
+              </p>
+              <p className="text-[20px]">19.99€</p>
+              <NavLink
+                className="bg-[#5390b3] rounded-2xl text-white w-full px-2 py-4 uppercase font-medium"
+                to={"/"}
+              >
+                Voir les détails
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="gap-4 flex flex-col  mb-32">
+        <h2 className="text-3xl font-medium">Vêtement </h2>
+        <div className="rounded-2xl p-4 flex flex-col gap-16 h-fit shadow-xl bg-[#F8F7F4]">
+          <div className="rounded-2xl p-2 h-fit shadow-xl bg-[#F8F7F4]">
+            <div className="text-center flex flex-col gap-4">
+              <img
+                className="shadow-2xl rounded-2xl w-full h-74 bg-fit object-cover"
+                src="https://img.freepik.com/psd-premium/boules-poids-pour-maquette-entrainement_439185-407.jpg?w=1480"
+                alt=""
+              />
+              <p className="text-2xl uppercase ">
+                Proteines maisons giehguiehg
+              </p>
+              <p className="text-[20px]">19.99€</p>
+              <NavLink
+                className="bg-[#5390b3] rounded-2xl text-white w-full px-2 py-4 uppercase font-medium"
+                to={"/"}
+              >
+                Voir les détails
+              </NavLink>
+            </div>
+          </div>
+          <div className="rounded-2xl p-2 h-fit shadow-xl bg-[#F8F7F4]">
+            <div className="text-center flex flex-col gap-4">
+              <img
+                className="shadow-2xl rounded-2xl w-full h-74 bg-fit object-cover"
+                src="https://img.freepik.com/psd-premium/boules-poids-pour-maquette-entrainement_439185-407.jpg?w=1480"
+                alt=""
+              />
+              <p className="text-2xl uppercase ">
+                Proteines maisons giehguiehg
+              </p>
+              <p className="text-[20px]">19.99€</p>
+              <NavLink
+                className="bg-[#5390b3] rounded-2xl text-white w-full px-2 py-4 uppercase font-medium"
+                to={"/"}
+              >
+                Voir les détails
+              </NavLink>
+            </div>
+          </div>
+          <div className="rounded-2xl p-2 h-fit shadow-xl bg-[#F8F7F4]">
+            <div className="text-center flex flex-col gap-4">
+              <img
+                className="shadow-2xl rounded-2xl w-full h-74 bg-fit object-cover"
+                src="https://img.freepik.com/psd-premium/boules-poids-pour-maquette-entrainement_439185-407.jpg?w=1480"
+                alt=""
+              />
+              <p className="text-2xl uppercase ">
+                Proteines maisons giehguiehg
+              </p>
+              <p className="text-[20px]">19.99€</p>
+              <NavLink
+                className="bg-[#5390b3] rounded-2xl text-white w-full px-2 py-4 uppercase font-medium"
+                to={"/"}
+              >
+                Voir les détails
+              </NavLink>
+            </div>
+          </div>
+          <div className="rounded-2xl p-2 h-fit shadow-xl bg-[#F8F7F4]">
+            <div className="text-center flex flex-col gap-4">
+              <img
+                className="shadow-2xl rounded-2xl w-full h-74 bg-fit object-cover"
+                src="https://img.freepik.com/psd-premium/boules-poids-pour-maquette-entrainement_439185-407.jpg?w=1480"
+                alt=""
+              />
+              <p className="text-2xl uppercase ">
+                Proteines maisons giehguiehg
+              </p>
+              <p className="text-[20px]">19.99€</p>
+              <NavLink
+                className="bg-[#5390b3] rounded-2xl text-white w-full px-2 py-4 uppercase font-medium"
+                to={"/"}
+              >
+                Voir les détails
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </section>
+      <button className="bg-[#5390b3] rounded-2xl text-white w-full px-2 py-4 uppercase font-medium">Voir plus</button>
+    </div>
+  );
+}
+
+export default Home;
