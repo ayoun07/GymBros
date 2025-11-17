94f2d7b46fc881f97f13ac50221aebd5df1bc870
ayoun07 <ayoun07>
2025-11-17 14:07:53 +0100

    First comment

---
+++ src/test/java/com/cwa/GymBros/GymBrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwa.GymBros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymBrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
