a880edd601bdafcc2d60dfdda7975bdef43f8c94
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-18 14:01:41 +0100

    appel du composent Header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
+import Header from './components/header/Header'
 function App() {
 
   return (
     <BrowserRouter>
+    <Header />
     <Routes>
       <Route path='/' Component={Home} />
     </Routes>
