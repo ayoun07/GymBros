2249b528aaeba3c57d3e41935633b98b449ab7b8
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-19 10:46:10 +0100

    update css button

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -9,7 +9,7 @@ export default function Button({ value, link }: ButtonProps) {
   return (
     <NavLink
       to={link || "/"}
-      className="text-center bg-[#5390b3] hover:bg-[#63acd6] transition duration-300 rounded-2xl text-white max-w-64 w-full px-2 py-4 uppercase font-medium"
+      className=" active:bg-[#63acd6] z-10 text-center bg-[#5390b3] hover:bg-[#63acd6] transition duration-300 rounded-2xl text-white max-w-64 w-full px-2 py-4 uppercase font-medium"
     >
       {value}
     </NavLink>
