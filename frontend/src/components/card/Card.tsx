f1eb091939596f3cc53de195a756a0f381eb834b
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-19 10:46:22 +0100

    update css card

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import Button from "../button/Button";
 
 interface CardProps {
@@ -9,19 +10,19 @@ interface CardProps {
 
 export default function Card({title, price, link, image}: CardProps) {
   return (
-    <div className="hover:scale-105 duration-300 transition text-center flex justify-center rounded-2xl max-w-80  p-2 h-fit shadow-xl bg-[#F8F7F4]">
+    <NavLink to={link} className="z-0 hover:scale-105 duration-300 transition text-center flex justify-center rounded-2xl max-w-80  p-2 h-fit shadow-xl bg-[#F8F7F4]">
       <div className=" flex flex-col gap-4">
         <img
           className="shadow-2xl rounded-2xl w-full h-56 bg-fit object-cover"
-          src={image}
+          src={image[0] || image}
           alt=""
         />
         <p className="font-semibold uppercase h-12 ">{title}</p>
         <p className="text-2xl font-semibold">{price.toFixed(2)} €</p>
         <div className="flex justify-center">
-       <Button value="ajouter au panier" link={link} />
+       <Button value="ajouter au panier" link={"/"} />
         </div>
       </div>
-    </div>
+    </NavLink>
   );
 }
