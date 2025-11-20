8a89d8b8fcc826c6b6ffc1872a5563d3c1279986
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-20 13:27:41 +0100

    création de la fonction d'ajout au panier

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,21 +1,32 @@
 import { FaShoppingCart } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
+import { addToCart } from "../../../service/CartService";
+import { useCart } from "../cart/CartContext";
 
 interface ButtonProps {
+  text: string;
   value: string;
   link?: string;
+  action?: string;
 }
 
-export default function Button({ value, link }: ButtonProps) {
+export default function Button({ text, value, link }: ButtonProps) {
+  const { refreshCart } = useCart();
+
+  const addCart = async () => {
+    await addToCart("1", value?.toString() as string);
+    await refreshCart();
+  };
+
+
   return (
     <NavLink
       to={link || "/"}
       className=" active:bg-[#63acd6] flex justify-center items-center gap-2 z-10 text-center bg-[#5390b3] hover:bg-[#63acd6] transition duration-300 rounded-2xl text-white max-w-64 w-full px-2 py-4 uppercase font-medium"
+      onClick={() => addCart()}
     >
-      {value}
-      {
-        value === "Ajouter au panier" && <FaShoppingCart size={24}/>
-      }
+      {text}
+      {text === "Ajouter au panier" && <FaShoppingCart size={24} />}
     </NavLink>
   );
 }
