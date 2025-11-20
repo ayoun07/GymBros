2db4cab2fa0a4944ef8f7741b68c16e72cccc5a7
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-20 13:28:05 +0100

    changement css

--- frontend/src/components/cart/ShoppingCart.tsx
+++ frontend/src/components/cart/ShoppingCart.tsx
@@ -8,7 +8,7 @@ export default function ShoppingCart() {
   return (
     <NavLink to={"/panier"} className="p-1 max-w-32 w-full relative">
       <FaShoppingCart className="mx-auto size-6 hover:text-[#63acd6] transition duration-150  " />
-      <p className="bg-white px-1 font-semibold text-sm border-2 border-gray-400 w-fit absolute bottom-0 left-5 rounded-4xl">{cartLength}</p>
+      <p className="bg-white px-1  font-semibold text-[10px] border-2 border-gray-400 w-fit absolute bottom-0 left-5 rounded-4xl">{cartLength}</p>
     </NavLink>
   );
 }
