2e92b29b79c9cc033be1e277ea410db2e3188c88
Lucas Bourdon <lb.lucasbourdon@gmail.com>
2025-11-20 09:35:24 +0100

    update logique return

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,3 +1,4 @@
+import { FaShoppingCart } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
 
 interface ButtonProps {
@@ -9,9 +10,12 @@ export default function Button({ value, link }: ButtonProps) {
   return (
     <NavLink
       to={link || "/"}
-      className=" active:bg-[#63acd6] z-10 text-center bg-[#5390b3] hover:bg-[#63acd6] transition duration-300 rounded-2xl text-white max-w-64 w-full px-2 py-4 uppercase font-medium"
+      className=" active:bg-[#63acd6] flex justify-center items-center gap-2 z-10 text-center bg-[#5390b3] hover:bg-[#63acd6] transition duration-300 rounded-2xl text-white max-w-64 w-full px-2 py-4 uppercase font-medium"
     >
       {value}
+      {
+        value === "Ajouter au panier" && <FaShoppingCart size={24}/>
+      }
     </NavLink>
   );
 }
